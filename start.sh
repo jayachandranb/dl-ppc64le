20719fb00374017e37f74f683b5d01c185717237
Jayachandran Balachandran <jayachandranbalachandran@Jcs-MacBook-Pro.local>
2017-11-20 18:41:52 +1100

    Fix Jupyter issue with root

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-jupyter notebook "$@" &! \
+jupyter notebook --allow-root "$@" &! \
 python -m tensorboard --logdir=/root/pod_storage/tf_logs --port=7777 --nopurge_orphaned_data --reload_interval=30
